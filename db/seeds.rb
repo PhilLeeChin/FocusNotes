9dcc4b0dee4e08300dafea947fbb5ef3624f30be
PhilLeeChin <phil.leechin@outlook.com>
2021-08-28 13:11:30 -0400

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# User.create!(name: 'Philip', username: 'pleechin', password: 'checksum')
+philip = User.first
+
+Notepad.create!(title: 'Feed Rex', note: 'Remember to feed Rex at 10:30AM.')
+
