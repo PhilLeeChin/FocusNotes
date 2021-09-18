1c69ec6df397087025fa511bc944500cc6e86d42
PhilLeeChin <phil.leechin@outlook.com>
2021-09-17 21:34:10 -0400

    add new method

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,5 +1,12 @@
 class Api::SessionsController < ApplicationController
+    def new
+
+    end
+
     def create
+        
+        # binding.pry
+        
         @user = User.find_by(username: params[:session][:username])
 
         if @user && @user.authenticate(params[:session][:passwords])
